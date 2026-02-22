44358342ca6df26232e1a44fb9ca9aefbad1e4cc
Bahram Haddadi <bahram_h@yahoo.com>
2026-02-22 14:38:50 -0500

    fix for message endpoint

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -29,8 +29,8 @@ export class ChatService {
   ): Observable<GetChatMessagesResponse> {
     const request: GetChatMessagesRequest = { orderId, pageNumber, pageSize };
     return this.apiService.post<GetChatMessagesResponse>(
-      '/Pharma/Support/GetMessages',
-      { body: request }
+      '/Pharma/User/GetOrderMessages',
+      { body: request.orderId }
     );
   }
 
